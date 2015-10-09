0e23ac739fc7ac23d6e9b4917564b3884e5b4150
Amanda Simon <mandysimon88@gmail.com>
2015-10-09 13:50:09 -0400

    Finished card class and specs

--- lib/card.rb
+++ lib/card.rb
@@ -3,27 +3,32 @@ class Card
   def initialize(attributes)
     @rank = attributes[:rank]
     @suit = attributes[:suit]
-    @face_up = true
+    @face_up = false # Amanda gets to find out why states are evil today!
   end
 
-  def face_up
+  def face_up?
     @face_up
   end
 
   def rank
-    @rank unless !face_up
+    @rank unless !face_up?
   end
 
   def suit
-    @suit unless !face_up
+    @suit unless !face_up?
   end
 
   def rank_numeric_value
     card_values = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"]
-    return card_values.index(@rank) + 2 unless !face_up
+    return card_values.index(@rank) + 2 unless !face_up?
   end
 
   def turn_over
-    @face_up = !@face_up
+    @face_up = !face_up?
+  end
+
+  def ==(another_card)
+    return false if !face_up? || !another_card.face_up? # Here's a great example of why states are evil!
+    return @rank == (another_card.rank) && @suit == (another_card.suit)
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,75 +2,114 @@ require 'spec_helper'
 
 describe(Card) do
   describe("#initialize") do
-    it('creates a face_up card with the provided rank and suit') do
-      myCard = Card.new(rank: "seven", suit: "clubs")
-      expect(myCard.rank).to eq "seven"
-      expect(myCard.suit).to eq "clubs"
-      expect(myCard.face_up).to eq true
+    it('creates a face down card with the provided rank and suit') do
+      my_card = Card.new(rank: "seven", suit: "clubs")
+      expect(my_card.face_up?).to eq false
+      my_card.turn_over
+      expect(my_card.rank).to eq "seven"
+      expect(my_card.suit).to eq "clubs"
     end
   end
 
-  describe("#face_up") do
+  describe("#face_up?") do
     it('returns true if the card is face_up') do
-      myCard = Card.new(rank: "seven", suit: "clubs")
-      expect(myCard.face_up).to eq true
+      my_card = Card.new(rank: "seven", suit: "clubs")
+      my_card.turn_over
+      expect(my_card.face_up?).to eq true
     end
     it('returns false if the card is face_down') do
-      myCard = Card.new(rank: "seven", suit: "clubs")
-      myCard.turn_over
-      expect(myCard.face_up).to eq false
+      my_card = Card.new(rank: "seven", suit: "clubs")
+      expect(my_card.face_up?).to eq false
     end
   end
 
   describe("#rank") do
     it('returns the card rank if it is face_up') do
-      myCard = Card.new(rank: "ace", suit: "spades")
-      expect(myCard.rank).to eq "ace"
+      my_card = Card.new(rank: "ace", suit: "spades")
+      my_card.turn_over
+      expect(my_card.rank).to eq "ace"
     end
     it('does nothing if the card is not face_up') do
-      myCard = Card.new(rank: "jack", suit: "diamonds")
-      myCard.turn_over
-      expect(myCard.rank).to eq nil
+      my_card = Card.new(rank: "jack", suit: "diamonds")
+      expect(my_card.rank).to eq nil
     end
   end
 
   describe("#suit") do
     it('returns the card suit if it is face_up') do
-      myCard = Card.new(rank: "king", suit: "hearts")
-      expect(myCard.rank).to eq "king"
+      my_card = Card.new(rank: "king", suit: "hearts")
+      my_card.turn_over
+      expect(my_card.rank).to eq "king"
     end
     it('does nothing if the card is not face_up') do
-      myCard = Card.new(rank: "jack", suit: "diamonds")
-      myCard.turn_over
-      expect(myCard.suit).to eq nil
+      my_card = Card.new(rank: "jack", suit: "diamonds")
+      expect(my_card.suit).to eq nil
     end
   end
 
   describe("#rank_numeric_value") do
     it('returns the numeric value of the rank') do
-      myCard = Card.new(rank: "seven", suit: "clubs")
-      expect(myCard.rank_numeric_value).to eq 7
+      my_card = Card.new(rank: "seven", suit: "clubs")
+      my_card.turn_over
+      expect(my_card.rank_numeric_value).to eq 7
     end
     it('works for face cards') do
-      myCard = Card.new(rank: "jack", suit: "clubs")
-      expect(myCard.rank_numeric_value).to eq 11
+      my_card = Card.new(rank: "jack", suit: "clubs")
+      my_card.turn_over
+      expect(my_card.rank_numeric_value).to eq 11
     end
     it('does nothing if the card is not face_up') do
-      myCard = Card.new(rank: "jack", suit: "diamonds")
-      myCard.turn_over
-      expect(myCard.rank_numeric_value).to eq nil
+      my_card = Card.new(rank: "jack", suit: "diamonds")
+      expect(my_card.rank_numeric_value).to eq nil
     end
   end
 
   describe('#turn_over') do
     it('toggles face_up between true or false') do
-      myCard = Card.new(rank: "eight", suit: "diamonds")
-      expect(myCard.face_up).to eq true
-      myCard.turn_over
-      expect(myCard.face_up).to eq false
-      myCard.turn_over
-      expect(myCard.face_up).to eq true
+      my_card = Card.new(rank: "eight", suit: "diamonds")
+      my_card.turn_over
+      expect(my_card.face_up?).to eq true
+      my_card.turn_over
+      expect(my_card.face_up?).to eq false
+      my_card.turn_over
+      expect(my_card.face_up?).to eq true
     end
   end
 
+  describe('#==') do
+    it('returns true for any two cards of the same rank and suit') do
+      my_card = Card.new(rank: "ten", suit: "diamonds")
+      my_card2 = Card.new(rank: "ten", suit: "diamonds")
+      my_card.turn_over
+      my_card2.turn_over
+      expect(my_card==my_card2).to be true
+    end
+    it('returns false if suit is different') do
+      my_card = Card.new(rank: "ten", suit: "hearts")
+      my_card2 = Card.new(rank: "ten", suit: "diamonds")
+      my_card.turn_over
+      my_card2.turn_over
+      expect(my_card==my_card2).to be false
+    end
+    it('returns false if rank is different') do
+      my_card = Card.new(rank: "four", suit: "clubs")
+      my_card2 = Card.new(rank: "five", suit: "clubs")
+      my_card.turn_over
+      my_card2.turn_over
+      expect(my_card==my_card2).to be false
+    end
+    it('returns false if both rank and suit are different') do
+      my_card = Card.new(rank: "nine", suit: "clubs")
+      my_card2 = Card.new(rank: "five", suit: "spades")
+      my_card.turn_over
+      my_card2.turn_over
+      expect(my_card==my_card2).to be false
+    end
+    it('returns false if either card is not face_up') do
+      my_card = Card.new(rank: "queen", suit: "hearts")
+      my_card2 = Card.new(rank: "three", suit: "hearts")
+      my_card2.turn_over
+      expect(my_card==my_card2).to be false
+    end
+  end
 end
