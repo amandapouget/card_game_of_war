5d252b41ede9922b4fb6cbe2559c806976e3ba8a
Amanda Simon <mandysimon88@gmail.com>
2015-10-12 14:09:10 -0400

    Provide possibility of anonymous default name

--- lib/player.rb
+++ lib/player.rb
@@ -2,33 +2,23 @@ class Player
 
   attr_reader :name, :cards
 
-  def initialize(attributes)
-    @name = attributes[:name]
+  def initialize(name: "Anonymous")
+    @name = name
     @cards = []
   end
 
   def play_next_card
     return if out_of_cards?
-    @cards[0].turn_over
     @cards.shift
-  end
-
-  def play_war_cards
-    war_cards = []
-    war_cards << @cards.shift
-    war_cards << play_next_card
-    war_cards
   end
 
   def collect_winnings(cards)
     cards.each do |card|
-      card.turn_over if card.face_up?
-      @cards << card
+      add_card(card)
     end
   end
 
   def add_card(card)
-    card.turn_over if card.face_up?
     @cards << card
   end
 
