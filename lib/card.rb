212f4e27f4dc3c06db66a0fae346684e56265bfd
Amanda Simon <mandysimon88@gmail.com>
2015-10-12 14:02:22 -0400

    Removed face_up attribute

--- lib/card.rb
+++ lib/card.rb
@@ -1,34 +1,18 @@
 class Card
 
-  def initialize(attributes)
-    @rank = attributes[:rank]
-    @suit = attributes[:suit]
-    @face_up = false # Amanda gets to find out why states are evil today!
+  attr_reader :rank, :suit
+
+  def initialize(rank:, suit:) # to assign defaults, use this syntax... rank: 'default string'
+    @rank = rank
+    @suit = suit
   end
 
-  def face_up?
-    @face_up
-  end
-
-  def rank
-    @rank unless !face_up?
-  end
-
-  def suit
-    @suit unless !face_up?
-  end
-
-  def rank_numeric_value # change to rank_value
+  def rank_value
     card_values = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"]
-    return card_values.index(@rank) + 2 unless !face_up?
-  end
-
-  def turn_over # this is something that belongs in the user interface not the state of the card itself
-    @face_up = !face_up?
+    return card_values.index(@rank) + 2
   end
 
   def ==(another_card)
-    return false if !face_up? || !another_card.face_up? # Here's a great example of why states are evil!
-    return @rank == (another_card.rank) && @suit == (another_card.suit)
+    return rank == (another_card.rank) && suit == (another_card.suit)
   end
 end
