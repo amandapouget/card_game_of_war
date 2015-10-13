f11e21b5d0824d6ee9d4e02d6efe54d70d9e6343
Amanda Simon <mandysimon88@gmail.com>
2015-10-13 16:58:03 -0400

    Server initialize, start, and ask_for_name all passing but having trouble killing threads

--- spec/war_server_spec.rb
+++ spec/war_server_spec.rb
@@ -10,21 +10,27 @@ ensure
 end
 
 describe WarServer do
-  context do
+  describe '#start' do
+    it 'starts up the server and thread' do
+      server_thread = Thread.new { WarServer.new(port: 2001).start }
+      client = MockWarClient.new(port: 2001) # don't understand why port numbers have to be different here
+      client.capture_output
+      expect(client.output).to eq "Welcome to war!\n"
+      server_thread.terminate # have to do this before joining thread or get stuck in the loop
+      puts server_thread.alive?
+      server_thread.join # have to join the thread in order to kill it
+      puts server_thread.alive?
+      server_thread.kill
+      puts server_thread.alive? # thread is dead, yet, port still occupied so have to use port 2000 on other tests below. UPDATE: thread isn't dead, just can't tell it's alive because you've joined it
+    end
+  end
+
+  context 'server created but not started' do
     let(:server) { WarServer.new(port: 2000)}
 
     describe '#initialize' do
       it 'creates a WarServer object with a server' do
         expect(server).to be_a WarServer
-      end
-    end
-
-    describe '#start' do
-      it 'starts up the server and thread' do
-        server_thread = Thread.new { WarServer.new(port: 2000).start }
-        client = MockWarClient.new(port: 2000)
-        client.capture_output
-        expect(client.output).to eq "Welcome to war!\n"
       end
     end
 
