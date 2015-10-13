afb2f5e086bc3292c15cc6d51087a304d34f29f8
Amanda Simon <mandysimon88@gmail.com>
2015-10-13 19:24:48 -0400

    All spec pass thread inside start, no before-after server thread create-exit for tests

--- lib/war_server.rb
+++ lib/war_server.rb
@@ -1,28 +1,38 @@
 require 'socket'
+require 'pry'
 
 class WarServer
-  attr_accessor :server, :socket
+  attr_accessor :server, :socket, :pending_clients, :clients
 
   def initialize(port:)
     @port = port
-    @socket = TCPServer.open(port)
+    @socket = TCPServer.open('localhost', port)
+    @pending_clients = []
+    @clients = []
   end
 
   def start
-    loop do
-      Thread.start(@socket.accept) do |client|
-        client.puts 'Welcome to war!'
+    Thread.new {
+      loop do
+        Thread.start(@socket.accept) do |client|
+          client.puts 'Welcome to war!'
+          @pending_clients << client
+          @pending_clients.length
+          if @pending_clients.length == 2
+              player1_socket = @pending_clients[0]
+              player2_socket = @pending_clients[1]
+              
+              player1_name = ask_for_name(client: player1_socket)
+              player2_name = ask_for_name(client: player2_socket)
+            @clients << @pending_clients.shift
+            @clients << @pending_clients.shift
+          end
+        end
       end
-      sleep(0.01)
-    end
+    }
   end
 
-  def ask_for_name(client: $stdout)
+  def ask_for_name(client:)
     client.puts "What is your name?"
   end
-
-  def start_game(player1, player2)
-    @game = Game.new()
-  end
-
 end
--- spec/war_server_spec.rb
+++ spec/war_server_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pry'
 
 def capture_stdout(&block)
   old = $stdout
@@ -15,7 +16,7 @@ describe WarServer do
       server_thread = Thread.new { WarServer.new(port: 2001).start }
       client = MockWarClient.new(port: 2001) # don't understand why port numbers have to be different here
       client.capture_output
-      expect(client.output).to eq "Welcome to war!\n"
+      expect(client.output).to eq ("Welcome to war!\n")
       server_thread.terminate # have to do this before joining thread or get stuck in the loop
       #puts server_thread.alive?
       server_thread.join # have to join the thread in order to kill it
@@ -25,20 +26,29 @@ describe WarServer do
     end
   end
 
-  context 'server created but not started' do
-    let(:server) { WarServer.new(port: 2000)}
-
-    describe '#initialize' do
-      it 'creates a WarServer object with a server' do
-        expect(server).to be_a WarServer
-      end
+  describe '#initialize' do
+    it 'creates a WarServer object with a server' do
+      server = WarServer.new(port: 2002)
+      expect(server).to be_a WarServer
     end
+  end
 
-    describe '#ask_for_name' do
-      it 'asks for the players name, gets it, and returns it' do
-        output = capture_stdout { server.ask_for_name }
-        expect(output).to eq "What is your name?\n"
-      end
+  describe '#ask_for_name' do
+    it 'asks for the players name, gets it, and returns it' do
+      server = WarServer.new(port: 2003)
+      output = capture_stdout { server.ask_for_name(client: $stdout) }
+      expect(output).to eq "What is your name?\n"
+    end
+  end
+
+  describe '#start' do
+    it 'start a game when there are two clients connected' do
+      server = WarServer.new(port: 2004)
+      server.start
+      client = MockWarClient.new(port: 2004)
+      client2 = MockWarClient.new(port: 2004)
+      sleep(3)
+      expect(server.clients.length).to eq 2
     end
   end
 end
