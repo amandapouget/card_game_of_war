1e7aaa1949a98acc270d341a7e496780b2df9caa
Amanda Simon <mandysimon88@gmail.com>
2015-10-23 09:43:14 -0400

    Removed extra comments in mock_war_client

--- lib/mock_war_client.rb
+++ lib/mock_war_client.rb
@@ -6,7 +6,6 @@ class MockWarClient
 
   def start
     @socket = TCPSocket.open('localhost', 2000)
-    # could put your JSON in here to load the welcome message and identifier given by accept in war_server
   end
 
   def provide_input(text)
@@ -24,17 +23,4 @@ class MockWarClient
     capture_output
     @my_output
   end
-
-  def discern(output)
-    my_hash = JSON.load(output)
-  end
 end
-
-=begin
-NEXT STEP IS TO MAKE CLIENT MODELED OFF OF MOCK CLIENT
-CLIENT MUST:
--- be able to discern JSON using .parse, .load (not recommended)
--- be able to spit things out in nice strings
--- be able to tell the difference between a match and a round_result, and also the difference between the match sent mid-play and that sent at the end of the Game
--- be able to have a user ID given by the server (do last)
-=end
